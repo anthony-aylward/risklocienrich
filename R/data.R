5c1df72ef18ffbe91bb4e689b2aa734b65c53794
Anthony Aylward <aaylward@eng.ucsd.edu>
2019-03-18 17:39:15 -0700

    update

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' Valid chromosome strings
+#'
+#' Vector of valid chromosome strings
+#'
+#' @format A character vector with 44 elements
+"valid_chromosomes"
--- R/risklocienrich.R
+++ R/risklocienrich.R
@@ -8,6 +8,7 @@
 #' @import GenomicRanges
 #' @import gwascat
 #' @import haploR
+#' @import regioneR
 
 
 
